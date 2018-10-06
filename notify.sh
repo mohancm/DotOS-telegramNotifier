87b3c0f8ced0644487e710cf1217faaa52644058
Mohan Cm <mohanmanjappa@gmail.com>
2018-10-06 19:19:18 +0530

    initial script for Telegram Notifier

---
+++ notify.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [[ -z "$TELEGRAM_BOT_API"  || -z "$TELEGRAM_CHAT_ID" ]]; then
+   echo -e "Please set \033[0;31mTELEGRAM_BOT_API"\033[0m and \033[0;31mTELEGRAM_CHAT_ID"\033[0m as enviorment variable"
+   exit 1
+fi
+
+TEXT="New Update for DotOS"
+
+
+curl -s -X POST https://api.telegram.org/bot$TELEGRAM_BOT_API/sendMessage -d chat_id=$TELEGRAM_CHAT_ID -d text="$TEXT"
